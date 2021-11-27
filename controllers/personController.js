f099fc54a1e24d95d2bcfe0f98d7e12d82a16ef9
tims-crew <timverstalshik@gmail.com>
2021-11-28 03:20:15 +0500

    feat: add createPerson

--- controllers/personController.js
+++ controllers/personController.js
@@ -1,5 +1,7 @@
 const Person = require('../modules/personModule');
 
+const { getPostData } = require('../utils')
+
 //@desc Gets all people
 //@route GET /api/person
 async function getPersons(req, res) {
@@ -38,17 +40,57 @@ async function getPerson(req, res, id) {
     }
 }
 
-async function createPerson(res, req, id) {
-    try {
-        const product = {
-            "name": "name",
-            "desc": "description"
-        };
+async function createPerson(res, req) {
+    try { 
+        const body = await getPostData(req);
 
-        const body = await Person.create(product);
+        if (!body || body === {}) {
+            console.log(body);
 
-        res.writeHead(201, { 'Content-Type': 'application/json'});
-        return res.end(JSON.stringify(body));        
+            res.writeHead(400, { 'Content-Type': 'plain/text' });
+            res.end('Please, write something before POSTing!!!');
+        } else {
+            const { name, age, hobbies } = JSON.parse(body);
+
+            const product = {
+                name,
+                age,
+                hobbies
+            };
+    
+            const newProduct = await Person.create(product);
+    
+            res.writeHead(201, { 'Content-Type': 'application/json'});
+            return res.end(JSON.stringify(newProduct));
+        }
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+async function updatePerson(res, req, id) {
+    try { 
+        const body = await getPostData(req);
+
+        if (!body || body === {}) {
+            console.log(body);
+
+            res.writeHead(400, { 'Content-Type': 'plain/text' });
+            res.end('Please, write something before updating!!!');
+        } else {
+            const { name, age, hobbies } = JSON.parse(body);
+
+            const product = {
+                name: name,
+                age: age,
+                hobbies: hobbies
+            };
+    
+            const updPersons = await Person.update(product, id);
+    
+            res.writeHead(201, { 'Content-Type': 'application/json'});
+            return res.end(JSON.stringify(updPersons));
+        }
     } catch(e) {
         console.error(e);
     }
@@ -57,5 +99,6 @@ async function createPerson(res, req, id) {
 module.exports = {
     getPersons,
     getPerson,
-    createPerson
+    createPerson,
+    updatePerson
 };
