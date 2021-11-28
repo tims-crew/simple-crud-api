2f06caf8087e42696fa5a01643c67b72afe9f135
tims-crew <timverstalshik@gmail.com>
2021-11-28 10:38:36 +0500

    feat: getPostData

---
+++ utils.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function writeDataToFile(filename, data) {
+    fs.writeFileSync(filename, JSON.stringify(data), 'utf8', (e) => {
+        console.error(e);
+    });
+}
+
+function getPostData(req) {
+    return new Promise((resolve, reject) => {
+        try {
+            let body = "";
+
+            req.on('data', chunk => {
+                body += chunk.toString();
+            })
+
+            req.on('end', () => {
+                resolve(body);
+            })
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+module.exports = {
+    writeDataToFile,
+    getPostData
+}
